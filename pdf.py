5d983902832f38372c868dee5b4005fba1ea03a0
sakkke <84666033+sakkke@users.noreply.github.com>
2024-01-16 18:28:16 +0900

    feat: add PDF-to-image feature

--- pdf.py
+++ pdf.py
@@ -1,4 +1,8 @@
 from discord.ext import commands
+from aiohttp import ClientSession
+from io import BytesIO
+from pdf2image import convert_from_bytes
+from discord import File
 
 class PDF(commands.Cog):
     def __init__(self, bot):
@@ -11,3 +15,28 @@ class PDF(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         print(f'{type(self).__name__}: Message from {message.author}: {message.content}')
+
+        for attachment in message.attachments:
+            if attachment.filename.endswith('.pdf'):
+                files = []
+                async with ClientSession() as session:
+                    async with session.get(attachment.url) as response:
+                        binary = await response.read()
+
+                        with BytesIO() as pdf_stream:
+                            pdf_stream.write(binary)
+                            pdf_stream.seek(0)
+                            pages = convert_from_bytes(pdf_stream.read())
+
+                            for i, page in enumerate(pages, 1):
+                                with BytesIO() as page_stream:
+                                    page.save(page_stream, 'JPEG')
+                                    page_stream.seek(0)
+                                    file = File(page_stream, f'image-{i}.jpg')
+                                    files.append(file)
+
+                for i, file in enumerate(files, 1):
+                    if i % 10 == 0:
+                        await message.channel.send(files=files[slice(i - 10, i)])
+                    elif len(files) == i:
+                        await message.channel.send(files=files[slice(i - i % 10, i)])
