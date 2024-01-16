25622cb8712047efdcefbb581f3db74aecfbc0c4
sakkke <84666033+sakkke@users.noreply.github.com>
2024-01-16 21:15:59 +0900

    feat: add HTTPd

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from discord import Bot, Intents
 import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
 
 from pdf import PDF
 
@@ -20,5 +22,13 @@ async def on_message(message):
     print(f'Message from {message.author}: {message.content}')
 
 bot.add_cog(PDF(bot))
+
+def run_httpd(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8080)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+httpd_thread = Thread(target=run_httpd, daemon=True)
+httpd_thread.start()
 
 bot.run(os.getenv('TOKEN'))
