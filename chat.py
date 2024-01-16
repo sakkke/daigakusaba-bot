11f2fa54b44966f09c2e722e5ce72ef400828e1d
sakkke <84666033+sakkke@users.noreply.github.com>
2024-01-16 22:17:18 +0900

    feat: add chat command

--- chat.py
+++ chat.py
@@ -1,8 +1,19 @@
 from discord.ext import commands
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.llm = ChatOpenAI()
+        prompt = ChatPromptTemplate.from_messages([
+            ('system', 'You are world class assistant. Please answer concisely and directly.'),
+            ('user', '{input}'),
+        ])
+        output_parser = StrOutputParser()
+        self.chain = prompt | self.llm | output_parser
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -11,3 +22,8 @@ class Chat(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         print(f'{type(self).__name__}: Message from {message.author}: {message.content}')
+
+    @commands.slash_command()
+    async def chat(self, ctx, text: str):
+        await ctx.defer()
+        await ctx.respond(self.chain.invoke({'input': text}))
